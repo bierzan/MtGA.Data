c24bfb918e01365d89fdf7447c4890c0f97b0117
bierzan@gmail.com <bierzan@gmail.com>
2019-06-25 23:04:10 +0200

    initial commit

---
+++ src/main/java/brzn/mtgadata/FileUploadController.java
@@ -0,0 +1,29 @@
+package brzn.mtgadata;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class FileUploadController {
+
+    @GetMapping("/")
+    public String listUploadedFiles()  {
+
+        return "uploadForm";
+    }
+
+    @PostMapping("/")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+
+        redirectAttributes.addFlashAttribute("message","You successfully uploaded " + file.getOriginalFilename() + "!");
+
+        return "redirect:/";
+    }
+
+
+}
---
+++ src/main/java/brzn/mtgadata/MtgaDataApp.java
@@ -0,0 +1,13 @@
+package brzn.mtgadata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgaDataApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtgaDataApp.class, args);
+    }
+
+}
